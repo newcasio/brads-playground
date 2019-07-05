833fafa5dcdf481bb5857ec3962ef1e88f929866
Brad <braddong@hotmail.com>
2019-07-05 21:00:36 +1000

    grid pics added

--- src/components/Mosaic.jsx
+++ src/components/Mosaic.jsx
@@ -3,14 +3,30 @@ import React from "react";
 function Mosaic() {
   return (
     <div className="mosaicWrapper">
-      <div className="box1">One</div>
-      <div className="box2">Two</div>
-      <div className="box3">Three</div>
-      <div className="box4">Four</div>
-      <div className="box5">Five</div>
-      <div className="box6">Six</div>
-      <div className="box7">Seven</div>
-      <div className="box8">Eight</div>
+      <div className="box box1 ">
+        <img src={require("./cat1.jpg")} alt="swimming in money" />
+      </div>
+      <div className="box box2">
+        <img src={require("./cat.png")} alt="samurai cat" />
+      </div>
+      <div className="box box3">
+        <img src={require("./cat1.jpg")} alt="swimming in money" />
+      </div>
+      <div className="box box4">
+        <img src={require("./cat.png")} alt="samurai cat" />
+      </div>
+      <div className="box box5">
+        <img src={require("./cat1.jpg")} alt="swimming in money" />
+      </div>
+      <div className="box box6">
+        <img src={require("./cat.png")} alt="samurai cat" />
+      </div>
+      <div className="box box7">
+        <img src={require("./cat1.jpg")} alt="swimming in money" />
+      </div>
+      <div className="box box8">
+        <img src={require("./cat.png")} alt="samurai cat" />
+      </div>
     </div>
   );
 }
